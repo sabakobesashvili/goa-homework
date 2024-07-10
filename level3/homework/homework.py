14dd66ef0b5d35d8ae0a246bd4ebbaf6da1a2fa7
sabakobesashvili <sabakobesashvili@gmail.com>
2024-07-10 18:38:43 +0400

    goa

--- level3/homework/homework.py
+++ level3/homework/homework.py
@@ -1,92 +1,111 @@
+# name = "saba"
+# lastnamne = "kobesashvili"
+# age = 11
+# hobby = "playing football"
+# country = "georgia"
+
+
+# print( name, lastnamne, age, hobby,country)
+
+
+
+
+
+
+
+
+
+
+
+# print(10 + 10)
+# print(23 + 120)
+# print(98 + 12)
+# print(87 + 20)
+# print(13 + 90)
+# print(3 + 109)
+# print(73 +12)
+# print(88 + 9)
+# print(72 + 981)
+# print(190 + 1202)
+
+
+
+
+
+
+# print(230-32)
+# print(98 -12)
+# print(23 - 13)
+# print(1302 - 912)
+# print(2937 - 2192)
+# print(3987 - 2131)
+# print(2013 - 2012)
+# print(902 - 192)
+# print(2012 - 2011)
+# print (100 - 44)
+
+
+
+
+# print(8972 / 2012)
+# print(829 / 20)
+# print(2089 / 201)
+# print(890 / 201)
+# print(9237/2910)
+# print(2817 / 122)
+# print(2920 / 222)
+# print(221 / 10)
+# print(2002 / 91)
+# print(1920/101)
+
+
+
+
+
+
+# print(10 *20)
+# print(72 * 29)
+# print(209 * 29)
+# print(27 * 10)
+# print(201 * 20)
+# print(18 * 71)
+# print(810 * 109)
+# print(103 * 192)
+# print(102 * 912)
+# print(87 * 891)
+
+
+
+
+
+
+
+
+
+# name = "gio"
+# lastname = "tatarashvili"
+# age = 21
+
+# name = "io"
+# lastname = "xizanishvili"
+# age = 14
+# print(age * age)
+
+
+
+
+
+
+
+
 name = "saba"
+age = "11"
 lastnamne = "kobesashvili"
-age = 11
-hobby = "playing football"
-country = "georgia"
-
-
-print( name, lastnamne, age, hobby,country)
-
-
-
-
-
-
-
-
-
-
-
-print(10 + 10)
-print(23 + 120)
-print(98 + 12)
-print(87 + 20)
-print(13 + 90)
-print(3 + 109)
-print(73 +12)
-print(88 + 9)
-print(72 + 981)
-print(190 + 1202)
-
-
-
-
-
-
-print(230-32)
-print(98 -12)
-print(23 - 13)
-print(1302 - 912)
-print(2937 - 2192)
-print(3987 - 2131)
-print(2013 - 2012)
-print(902 - 192)
-print(2012 - 2011)
-print (100 - 44)
-
-
-
-
-print(8972 / 2012)
-print(829 / 20)
-print(2089 / 201)
-print(890 / 201)
-print(9237/2910)
-print(2817 / 122)
-print(2920 / 222)
-print(221 / 10)
-print(2002 / 91)
-print(1920/101)
-
-
-
-
-
-
-print(10 *20)
-print(72 * 29)
-print(209 * 29)
-print(27 * 10)
-print(201 * 20)
-print(18 * 71)
-print(810 * 109)
-print(103 * 192)
-print(102 * 912)
-print(87 * 891)
-
-
-
-
-
-
-
-
-
-name = "gio"
-lastname = "tatarashvili"
-age = 21
-
-name = "io"
-lastname = "xizanishvili"
-age = 14
-print(age * age)
+hobby = "cook"
+weight = "40"
+height = "1.51"
+favoritesport = "volleyball"
+favoritefruit = "strawberry"
+favoritepet = "dog"
+favoritefilm = "Avengers"
+print(name,age,lastnamne,hobby,weight,height,favoritesport,favoritefruit,favoritepet,favoritefilm)
