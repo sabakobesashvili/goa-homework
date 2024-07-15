0386ad1e57e5662c3930db0be4c250ef8569d932
sabakobesashvili <sabakobesashvili@gmail.com>
2024-07-15 14:45:49 +0400

    jsdhasu

--- level3/classwork/classwork.py
+++
@@ -1,9 +0,0 @@
-#task 1
-age = 36
-print(age + 5)
-#task 2
-myage = 11
-print(myage)
-myage = 36
-print(myage)
-
---
+++ level3/classwork/cw.py
@@ -0,0 +1,8 @@
+age = 36
+print(age + 5)
+
+myage = 11
+
+print (myage)
+myage = 36
+print(myage)
