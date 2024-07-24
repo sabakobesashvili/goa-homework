5115cdd2877e67744e2a2f8ac32ce333c05bb077
sabakobesashvili <sabakobesashvili@gmail.com>
2024-07-24 18:13:27 +0400

    goa

---
+++ level5/classwork/classwork.py
@@ -0,0 +1,29 @@
+name = "Alice"
+age = 25
+height = 5.7
+weight = 60.5
+hobby = "reading"
+
+print(name)
+print(age)
+print(height)
+print(weight)
+print(hobby)
+
+new_age = age + 1
+prin(new_age)
+
+full_height = height, "meter"
+print(full_height)
+
+description = name +   +  age + yearsold and likes  +  hobby
+print(description)
+
+new_weight = weight - 5.0
+print(New_Weight, new_weight)
+
+message = Hello + 123
+print(message)
+
+age_next_year = age + 1
+print(Age , NextYear, age_next_year)
---
+++ level5/homework/homework.py
@@ -0,0 +1,34 @@
+# name = saba #name = "saba"
+# lastnamne = kobesashvili #lastname = "kobesashvili"
+# age = "11" #age = 11
+# hobby = playingfootball #hobby = "playingfootball"
+# print("hobby") #print(hobby)
+# print("lastnamne")  #print(lastnamne)
+# print("name") #print(name)
+# print("age") #print(age)
+  
+
+
+
+# print(food) #print("food")
+# print(apple) #print("apple")
+# print(keybord) #print("keybord")
+# print(mouse) #print("mouse")
+# print(house) #print("house")
+# print(videogame) #print("videogame")
+# print(goa) #print("goa")
+# print(academy) #print("academy")
+# print(sololearn) #print("sololearn")
+# print(code) #print("code")
+# print(developer) #print("developer")
+# print(study) #print("study")
+
+
+
+
+
+
+name = "saba"
+age = 11
+print("hello",name,"you are",age,"years old" )
+
